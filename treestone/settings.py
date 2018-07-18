66782fe1a651a9f06b22af95ca4e847cd638ebbc
Abigail Rettew <abigailrettew@nat-tvwna-inside-visitornet101-e-219.princeton.org>
2018-07-18 16:14:47 -0400

    Changed database settings

--- treestone/settings.py
+++ treestone/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'treestone.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {dj_database_url.config(
+    'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 }

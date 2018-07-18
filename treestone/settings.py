e280a503da1f9cc3c47d3887c6523e5a3efbc375
Abigail Rettew <abigailrettew@nat-tvwna-inside-visitornet101-e-219.princeton.org>
2018-07-18 16:15:29 -0400

    Added import

--- treestone/settings.py
+++ treestone/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'treestone.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')

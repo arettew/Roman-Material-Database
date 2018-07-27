04eb5b23a79417b83b7c8dae86be6e21174256e5
Abigail Rettew <abigailrettew@nat-oitwireless-inside-vapornet100-c-17220.Princeton.EDU>
2018-07-27 11:46:00 -0400

    Re-added editing

--- treestone/tree/views.py
+++ treestone/tree/views.py
@@ -285,7 +285,7 @@ class StoneUpdateView(LoginRequiredMixin, UpdateView):
   def form_valid(self, form):
     edit = StoneEdits()
     stone = Stones.objects.get(pk=self.object.pk)
-    #helper.createEdit('stones', stone, form.cleaned_data, self.request.user)
+    helper.createEdit('stones', stone, form.cleaned_data, self.request.user)
 
     return HttpResponseRedirect('/success/')
 
